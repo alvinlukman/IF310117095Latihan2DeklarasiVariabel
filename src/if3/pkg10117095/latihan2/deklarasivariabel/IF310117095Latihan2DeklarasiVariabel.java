eb097a8f4f7b6b973b32861c101af7cb3a3c9ae7
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:22:50 +0700

    DeklarasiVariabel

---
+++ src/if3/pkg10117095/latihan2/deklarasivariabel/IF310117095Latihan2DeklarasiVariabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Deklarasi dan Pengaksesan Variabel
+ */
+public class IF310117095Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta upsercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memeberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel Logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
